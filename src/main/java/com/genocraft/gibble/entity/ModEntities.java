34931a6de61b5a9fe18597ff5bcf71aed5ce8e31
Genocraft <gynocraft@gmail.com>
2026-08-29 16:46:33 +0200

    made GibbleEntity work

--- src/main/java/com/genocraft/gibble/entity/ModEntities.java
+++ src/main/java/com/genocraft/gibble/entity/ModEntities.java
@@ -1,4 +1,25 @@
 package com.genocraft.gibble.entity;
 
+import com.genocraft.gibble.entity.custom.GibbleEntity;
+import com.genocraft.gibble.gibble;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
 public class ModEntities {
+    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
+            DeferredRegister.create(BuiltInRegistries.ENTITY_TYPE, gibble.MODID);
+
+    //  Width and height have not been tested yet. Need to correct it.
+    public static final Supplier<EntityType<GibbleEntity>> GIBBLE =
+            ENTITY_TYPES.register("gibble", () -> EntityType.Builder.of(GibbleEntity::new, MobCategory.CREATURE)
+                    .sized(8f, 8f).build("gibble"));
+
+    public static void register(IEventBus eventBus) {
+        ENTITY_TYPES.register((eventBus));
+    }
 }
--- src/main/java/com/genocraft/gibble/entity/custom/GibbleEntity.java
+++ src/main/java/com/genocraft/gibble/entity/custom/GibbleEntity.java
@@ -1,4 +1,70 @@
 package com.genocraft.gibble.entity.custom;
 
-public class GibbleEntity {
+import com.genocraft.gibble.entity.ModEntities;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.AnimationState;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+public class GibbleEntity extends Animal {
+    public final AnimationState idleAnimationState = new AnimationState();
+    private int idleAnimationTimeout = 0;
+
+    public GibbleEntity(EntityType<? extends Animal> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new TemptGoal(this, 1.50, stack -> stack.is(Items.COCOA_BEANS), false));
+        this.goalSelector.addGoal(2, new LookAtPlayerGoal(this, Player.class, 6.0F));
+        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return Animal.createLivingAttributes()
+                .add(Attributes.MAX_HEALTH, 10d)
+                .add(Attributes.MOVEMENT_SPEED, 1D)
+                .add(Attributes.FOLLOW_RANGE, 24D)
+                .add(Attributes.ARMOR, 1d)
+                .add(Attributes.ARMOR_TOUGHNESS, 1d);
+    }
+
+    private void setupAnimationStates() {
+        if(this.idleAnimationTimeout <= 0) {
+            this.idleAnimationTimeout = 80;
+            this.idleAnimationState.start(this.tickCount);
+        } else {
+            --this.idleAnimationTimeout;
+        }
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        if(this.level().isClientSide()) {
+            this.setupAnimationStates();
+        }
+    }
+
+    @Override
+    public boolean isFood(ItemStack itemStack) {
+        return false;
+    }
+
+    @Override
+    public @Nullable AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+        return ModEntities.GIBBLE.get().create(serverLevel);
+    }
 }
